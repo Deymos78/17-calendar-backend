e2adf1407930b2d2b8947dc85122f776afe56d34
Andres <dandres159@gmail.com>
2023-06-26 13:45:02 +0200

    Nuevos cambios

--- controllers/auth.js
+++ controllers/auth.js
@@ -130,6 +130,7 @@ const revalidarToken = async( req, res = express.response ) => {
 
     res.json({
         ok: true,
+        uid, name,
         token
     });
 }
--- middlewares/validar-campos.js
+++ middlewares/validar-campos.js
@@ -7,13 +7,13 @@ const validarCampos = ( req, res = express.response, next ) => {
     const errors = validationResult( req );
 
     if( !errors.isEmpty() ){
-        return res.json({
+        return res.status(400).json({
             ok: false,
             errors: errors.mapped()
         })
     }
 
-    next();
+    next(); 
 }
 
 
